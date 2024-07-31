c3ff7934ba9be37dc2de10db07e2f601f02fa0ec
Harishwaran2002 <146076518+Harishwaran2002@users.noreply.github.com>
2024-07-31 12:46:27 +0530

    Fibonacci sequence.java

---
+++ Fibonacci sequence.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=0;
+        int b=1;
+        int c=0;
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.print(a+" ");
+        c=a+b;
+        b=a;
+        a=c;
+        }
+            
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
